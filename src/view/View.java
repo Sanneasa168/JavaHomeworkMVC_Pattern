733e88b0a46abe0e0627c628fb691e96171cc8d7
Neasa san <nongsuykady2023@gmail.com>
2024-06-04 19:25:58 +0700

    first commite

---
+++ src/view/View.java
@@ -0,0 +1,28 @@
+package view;
+
+import controller.UserController;
+import model.User;
+
+public class View {
+    private static final UserController userController = new UserController();
+    public static void UI(){
+        System.out.println("This is views :");
+        userController.getAllUser()
+                .forEach(System.out::println);
+        System.out.println("=".repeat(50));
+
+        userController.addNewUser(new User(3,"Maly","Maly@gmail.com"));
+        System.out.println("After adding new user :");
+        userController.getAllUser().forEach(System.out::println);
+        System.out.println("=".repeat(50));
+
+        System.out.println("Deleted User :");
+        userController.deleteUser(2).forEach(System.out::println);
+
+        System.out.println("=".repeat(50));
+        System.out.println("Successfully deleted User :");
+        userController.getAllUser()
+                .forEach(System.out::println);
+        System.out.println("=".repeat(50));
+    }
+}
